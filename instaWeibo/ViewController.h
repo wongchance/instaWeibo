f8ccff851450630cf1f820c1afd07e1275b6ed23
王超 <wangchao@wangxiaochao-MacBook-Pro.local>
2015-07-25 22:05:12 +0800

    Initial Commit

---
+++ instaWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instaWeibo
+//
+//  Created by 王超 on 15/7/25.
+//  Copyright (c) 2015年 wongchance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
